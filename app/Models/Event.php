e3aae9373df92d3e7abbbbb063bcd4733dfef45d
VargaDoro <varga.dorothea.timea@gmail.com>
2026-01-10 23:33:44 +0100

    events, participates

---
+++ app/Models/Event.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    /** @use HasFactory<\Database\Factories\EventFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'agency_id',
+        'limit',
+        'date',
+        'location',
+        'status'
+    ];
+}
---
+++ app/Models/Participate.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Participate extends Model
+{
+    /** @use HasFactory<\Database\Factories\ParticipateFactory> */
+    use HasFactory;
+    protected $fillable=[
+        'event_id',
+        'user_id',
+        'present'
+    ];
+    
+}
