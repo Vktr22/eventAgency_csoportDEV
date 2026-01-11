1f1ebf87163de44642afd22087cff29ec13f0af6
VargaDoro <varga.dorothea.timea@gmail.com>
2026-01-11 11:47:46 +0100

    users api megjelenik

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-//Route::get('/users', [UserController::class, 'index']);        // Lista      //az adott class-nak az adott függvénye az index
-//Route::get('/users/{user}', [UserController::class, 'show']); // Egy user
-//Route::post('/users', [UserController::class, 'store']);      // Létrehozás
-//Route::put('/users/{user}', [UserController::class, 'update']); // Frissítés
-//Route::delete('/users/{user}', [UserController::class, 'destroy']); // Törlés
+Route::get('/users', [UserController::class, 'index']);        // Lista      //az adott class-nak az adott függvénye az index
+Route::get('/users/{user}', [UserController::class, 'show']); // Egy user
+Route::post('/users', [UserController::class, 'store']);      // Létrehozás
+Route::put('/users/{user}', [UserController::class, 'update']); // Frissítés
+Route::delete('/users/{user}', [UserController::class, 'destroy']); // Törlés
