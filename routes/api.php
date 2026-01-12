c71d22647596689da5bcb00e7cc9da4b991f194e
Eszti <nagy.eszter2222@gmail.com>
2026-01-12 14:22:15 +0100

    vmi

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgencyController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,16 @@ Route::get('/users/{user}', [UserController::class, 'show']); // Egy user
 Route::post('/users', [UserController::class, 'store']);      // Létrehozás
 Route::put('/users/{user}', [UserController::class, 'update']); // Frissítés
 Route::delete('/users/{user}', [UserController::class, 'destroy']); // Törlés
+
+
+
+
+// Ügynökségek végpontjai
+Route::get('/agencies', [AgencyController::class, 'index']);        // Lista
+Route::get('/agencies/{agency}', [AgencyController::class, 'show']); // Egy ügynökség
+Route::post('/agencies', [AgencyController::class, 'store']);        // Létrehozás
+Route::put('/agencies/{agency}', [AgencyController::class, 'update']); // Frissítés
+Route::delete('/agencies/{agency}', [AgencyController::class, 'destroy']); // Törlés
+
+
+
